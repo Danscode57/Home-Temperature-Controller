0eed93c23c537f66a05d3074254568f3a1515a9e
Greg Gigon <greg.gigon@gmail.com>
2016-03-08 13:08:00 +0000

    Fixed slider

--- ui/mobile-web-ui/src/components/schedule/HourComponent.js
+++ ui/mobile-web-ui/src/components/schedule/HourComponent.js
@@ -1,9 +1,9 @@
 require('normalize.css');
 require('styles/App.css');
 
+import R from 'ramda';
 import React, { PropTypes, Component } from 'react';
 import TimePicker from 'material-ui/lib/time-picker/time-picker';
-
 import Slider from 'material-ui/lib/slider';
 
 import FloatingActionButton from 'material-ui/lib/floating-action-button';
@@ -13,6 +13,23 @@ class HourComponent extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {dragStarted: true, temp: props.hour.temp};
+    }
+
+    handleDragStop(updateTempAction, day, index) {
+        if (this.state.dragStarted) {
+            this.setState({dragStarted: false});
+            let newValue = this.refs.tempSlider.getValue();
+            updateTempAction(day, index, newValue);
+        }
+    }
+
+    handleOnChange(updateTempAction, day, index, newValue) {
+        if (!this.state.dragStarted) {
+            updateTempAction(day, index, newValue);
+        } else {
+            this.setState(R.assoc('temp', newValue)(this.state))
+        }
     }
 
     render() {
@@ -20,6 +37,7 @@ class HourComponent extends Component {
         let temp = hour.temp;
         let time = hour.time;
         let defaultTime = new Date(Date.parse('1979-10-10 ' + time));
+
 
         return (
             <div>
@@ -29,7 +47,7 @@ class HourComponent extends Component {
                                     onChange={ (e,t)=> actions.updateTime(day, index, t)}/>
                     </div>
                     <div className="scheduleHourItem">
-                        <input type="text" value={temp} style={{width: 40}} readOnly={true}/>&deg;C
+                        <input type="text" value={this.state.temp} style={{width: 40}} readOnly={true}/>&deg;C
                     </div>
                     <div className="scheduleHourItem">
                         <FloatingActionButton mini={true} onClick={ () => actions.removeHourSchedule(day, index)}>
@@ -38,7 +56,10 @@ class HourComponent extends Component {
                     </div>
                 </div>
 
-                <Slider step={0.5} defaultValue={temp} min={15} max={23} onChange={(e,v) => actions.updateTemp(day, index, v)} />
+                <Slider step={0.5} defaultValue={temp} min={15} max={23} ref="tempSlider"
+                        onDragStart={() => this.setState(R.assoc('dragStarted', true)(this.state))}
+                        onDragStop={ (e) => this.handleDragStop(actions.updateTemp, day, index)}
+                        onChange={(e,v) => this.handleOnChange(actions.updateTemp, day, index, v)}/>
 
             </div>
         );
