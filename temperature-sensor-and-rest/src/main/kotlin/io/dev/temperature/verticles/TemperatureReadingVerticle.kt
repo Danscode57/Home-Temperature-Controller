4012a4f16501f4562e329be8acccf1a82d8dfd56
Greg Gigon <greg.gigon@gmail.com>
2016-06-22 09:29:52 +0100

    Simplified the logic

--- temperature-sensor-and-rest/src/main/kotlin/io/dev/temperature/verticles/TemperatureReadingVerticle.kt
+++ temperature-sensor-and-rest/src/main/kotlin/io/dev/temperature/verticles/TemperatureReadingVerticle.kt
@@ -12,6 +12,7 @@ import java.io.FileNotFoundException
  * Created by gigu on 18/06/2016.
  */
 class TemperatureReadingVerticle(val w1FileLocation: String = "/sys/bus/w1/devices", val frequency: Float = 5f) : AbstractVerticle() {
+    var periodicTimer: Long = 0
 
     companion object {
         val FILE_SEARCH_PATTERN = "28-"
@@ -20,35 +21,43 @@ class TemperatureReadingVerticle(val w1FileLocation: String = "/sys/bus/w1/devic
         val STAMP_REGEX = Regex(".*crc=(\\d|\\w{0,2})\\sYES", setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE))
     }
 
+    override fun stop() {
+        vertx.cancelTimer(periodicTimer)
+    }
+
     override fun start(startFuture: Future<Void>?) {
         val (sensorPresent, sensorFolder) = deviceFolderPresent()
+
         if (!sensorPresent) {
             startFuture!!.fail(FileNotFoundException("Cannot find sensor Folders and files in location ${w1FileLocation}"))
-        } else {
-            val sensorFile = File(sensorFolder, SENSOR_FILE_NAME)
-            if (!sensorFile.exists()) {
-                startFuture!!.fail(FileNotFoundException("Sensor file doesn't exist ${sensorFile.absolutePath}"))
-            } else {
-                val delay: Long = (frequency * 1000).toLong()
-                val taskId = vertx.setPeriodic(delay) { delay ->
-                    try {
-                        val readTemperatureFromFile = readTemperatureFromFile(sensorFile)
+            return
+        }
 
-                        if (readTemperatureFromFile != null) {
-                            val receivedReading = JsonObject().
-                                    put("temperature", readTemperatureFromFile.component1()).
-                                    put("stamp", readTemperatureFromFile.component2())
+        val sensorFile = File(sensorFolder, SENSOR_FILE_NAME)
+        if (!sensorFile.exists()) {
+            startFuture!!.fail(FileNotFoundException("Sensor file doesn't exist ${sensorFile.absolutePath}"))
+            return
+        }
 
-                            vertx.eventBus().publish(BusAddresses.TemperatureReadings.TEMPERATUR_READING_RECEIVED, receivedReading)
-                        }
-                    } catch(exception: FileNotFoundException) {
-                        val message = JsonObject().put("code", 0).put("message", exception.message)
-                        vertx.eventBus().publish(BusAddresses.TemperatureReadings.TEMPERATURE_SENSOR_READING_FAILED, message)
-                    }
+        val delay: Long = (frequency * 1000).toLong()
+        periodicTimer = vertx.setPeriodic(delay) { delay ->
+            try {
+                val readTemperatureFromFile = readTemperatureFromFile(sensorFile)
+
+                if (readTemperatureFromFile != null) {
+                    val receivedReading = JsonObject().
+                            put("temperature", readTemperatureFromFile.component1()).
+                            put("stamp", readTemperatureFromFile.component2())
+
+                    vertx.eventBus().publish(BusAddresses.TemperatureReadings.TEMPERATUR_READING_RECEIVED, receivedReading)
                 }
-                startFuture!!.complete()
+            } catch(exception: FileNotFoundException) {
+                val message = JsonObject().put("code", 0).put("message", exception.message)
+                vertx.eventBus().publish(BusAddresses.TemperatureReadings.TEMPERATURE_SENSOR_READING_FAILED, message)
             }
         }
+        startFuture!!.complete()
+
 
     }
 
