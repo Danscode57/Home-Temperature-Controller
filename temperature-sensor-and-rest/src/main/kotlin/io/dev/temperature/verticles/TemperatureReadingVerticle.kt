2f2ca24c11908e9f27d334ac62dbfbe1ed2fbdf9
Greg Gigon <greg.gigon@gmail.com>
2016-06-22 09:26:06 +0100

    Sensing temperature and sending events

--- temperature-sensor-and-rest/src/main/kotlin/io/dev/temperature/verticles/TemperatureReadingVerticle.kt
+++ temperature-sensor-and-rest/src/main/kotlin/io/dev/temperature/verticles/TemperatureReadingVerticle.kt
@@ -1,41 +1,74 @@
 package io.dev.temperature.verticles
 
+import io.dev.temperature.BusAddresses
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.json.JsonObject
 import kotlinx.util.with
 import java.io.File
+import java.io.FileNotFoundException
 
 /**
  * Created by gigu on 18/06/2016.
  */
-class TemperatureReadingVerticle(val w1FileLocation: String = "/sys/bus/w1/devices", val frequency: Int = 5): AbstractVerticle() {
+class TemperatureReadingVerticle(val w1FileLocation: String = "/sys/bus/w1/devices", val frequency: Float = 5f) : AbstractVerticle() {
 
     companion object {
         val FILE_SEARCH_PATTERN = "28-"
         val SENSOR_FILE_NAME = "w1_slave"
         val TEMP_REGEX = Regex(".*\\st=(\\d{4,5})", setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE))
+        val STAMP_REGEX = Regex(".*crc=(\\d|\\w{0,2})\\sYES", setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE))
     }
 
     override fun start(startFuture: Future<Void>?) {
+        val (sensorPresent, sensorFolder) = deviceFolderPresent()
+        if (!sensorPresent) {
+            startFuture!!.fail(FileNotFoundException("Cannot find sensor Folders and files in location ${w1FileLocation}"))
+        } else {
+            val sensorFile = File(sensorFolder, SENSOR_FILE_NAME)
+            if (!sensorFile.exists()) {
+                startFuture!!.fail(FileNotFoundException("Sensor file doesn't exist ${sensorFile.absolutePath}"))
+            } else {
+                val delay: Long = (frequency * 1000).toLong()
+                val taskId = vertx.setPeriodic(delay) { delay ->
+                    try {
+                        val readTemperatureFromFile = readTemperatureFromFile(sensorFile)
+
+                        if (readTemperatureFromFile != null) {
+                            val receivedReading = JsonObject().
+                                    put("temperature", readTemperatureFromFile.component1()).
+                                    put("stamp", readTemperatureFromFile.component2())
+
+                            vertx.eventBus().publish(BusAddresses.TemperatureReadings.TEMPERATUR_READING_RECEIVED, receivedReading)
+                        }
+                    } catch(exception: FileNotFoundException) {
+                        val message = JsonObject().put("code", 0).put("message", exception.message)
+                        vertx.eventBus().publish(BusAddresses.TemperatureReadings.TEMPERATURE_SENSOR_READING_FAILED, message)
+                    }
+                }
+                startFuture!!.complete()
+            }
+        }
 
     }
 
     fun deviceFolderPresent(): Pair<Boolean, File?> {
         val possibleFolders = File(w1FileLocation).listFiles { file, path -> path.startsWith(FILE_SEARCH_PATTERN) }
-        if (possibleFolders.size == 0){
+        if (possibleFolders.size == 0) {
             return Pair(false, null)
         }
         return Pair(true, possibleFolders.first())
     }
 
-    fun readTemperatureFromFile(deviceFolder: File): Float? {
-        File(deviceFolder, SENSOR_FILE_NAME).with {
+    fun readTemperatureFromFile(deviceFile: File): Pair<Float, String>? {
+        deviceFile.with {
             val fileContent = readText()
+            val stampMatch = STAMP_REGEX.find(fileContent) ?: return null
             val match = TEMP_REGEX.find(fileContent) ?: return null
 
-            if (match.groups.size > 1){
+            if (match.groups.size > 1) {
                 val fromFile = match.groupValues.last()
-                return "${fromFile.subSequence(0, 2)}.${fromFile.subSequence(2, fromFile.length)}".toFloat()
+                return Pair("${fromFile.subSequence(0, 2)}.${fromFile.subSequence(2, fromFile.length)}".toFloat(), stampMatch.groupValues.last())
             }
         }
         return null
--- temperature-sensor-and-rest/src/test/kotlin/io/dev/temperature/verticles/TemperatureReadingVerticleTest.kt
+++ temperature-sensor-and-rest/src/test/kotlin/io/dev/temperature/verticles/TemperatureReadingVerticleTest.kt
@@ -1,5 +1,10 @@
 package io.dev.temperature.verticles
 
+import io.dev.temperature.BusAddresses
+import io.vertx.core.DeploymentOptions
+import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
 import kotlinx.util.with
 import org.junit.*
@@ -17,6 +22,9 @@ class TemperatureReadingVerticleTest {
         val TEST_PATH_LOCATION = "~/tmp/temptests"
         var DEVICE_NAME_FOLDER = "28-8000001eaad4"
 
+        val vertx = Vertx.vertx()
+        val WORKER_VERTICLE = DeploymentOptions().setWorker(true)
+
         @BeforeClass @JvmStatic fun setUp() {
             File(TEST_PATH_LOCATION).mkdirs()
         }
@@ -27,26 +35,102 @@ class TemperatureReadingVerticleTest {
     }
 
     @Before
-    fun makeSureFoldersAreCleared(){
-         File(TEST_PATH_LOCATION).listFiles().forEach { it.deleteRecursively() }
+    fun makeSureFoldersAreCleared() {
+        File(TEST_PATH_LOCATION).listFiles().forEach { it.deleteRecursively() }
+        vertx.deploymentIDs().forEach { vertx.undeploy(it) }
     }
 
-    @Test fun shouldReturnFilePathIfLocationIsValid(){
+    @Test fun shouldReturnFilePathIfLocationIsValid() {
         File(TEST_PATH_LOCATION, DEVICE_NAME_FOLDER).mkdirs()
 
         val verticle = TemperatureReadingVerticle(w1FileLocation = TEST_PATH_LOCATION)
-        val (result, file) =  verticle.deviceFolderPresent()
+        val (result, file) = verticle.deviceFolderPresent()
 
         assert(result)
     }
 
-    @Test fun shouldReturnFalseIfDeviceFolderIsNotInPath(){
+    @Test fun shouldReturnFalseIfDeviceFolderIsNotInPath() {
         val verticle = TemperatureReadingVerticle(w1FileLocation = TEST_PATH_LOCATION)
         val (result, file) = verticle.deviceFolderPresent()
         assert(!result)
     }
 
-    @Test fun shouldReadReadTemperatureFromFile(){
+    @Test fun shouldReadTemperatureFromFile() {
+        val directory = File(TEST_PATH_LOCATION, DEVICE_NAME_FOLDER)
+        directory.mkdirs()
+
+        val sensorFile = File(directory, TemperatureReadingVerticle.SENSOR_FILE_NAME)
+        sensorFile.with {
+            writeText(SAMPLE_VALID_FILE_CONTENT)
+        }
+
+        val verticle = TemperatureReadingVerticle(TEST_PATH_LOCATION)
+
+        val result = verticle.readTemperatureFromFile(sensorFile)
+
+        Assert.assertNotNull(result)
+        Assert.assertEquals(20.437f, result!!.component1())
+        Assert.assertEquals("6f", result.component2())
+    }
+
+    @Test(expected = FileNotFoundException::class) fun shouldFailIfFileDoesntExists() {
+        val directory = File(TEST_PATH_LOCATION, DEVICE_NAME_FOLDER)
+
+        val verticle = TemperatureReadingVerticle(TEST_PATH_LOCATION)
+
+        val result = verticle.readTemperatureFromFile(directory)
+
+        Assert.assertNull(result)
+    }
+
+    @Test fun shouldReturnNullIfBoloxInFile() {
+        val directory = File(TEST_PATH_LOCATION, DEVICE_NAME_FOLDER)
+        directory.mkdirs()
+
+        val sensorFile = File(directory, TemperatureReadingVerticle.SENSOR_FILE_NAME)
+
+        sensorFile.with {
+            writeText("BALLS some lore ipsum shit and stuff")
+        }
+        val verticle = TemperatureReadingVerticle(TEST_PATH_LOCATION)
+
+        val result = verticle.readTemperatureFromFile(sensorFile)
+        Assert.assertNull(result)
+    }
+
+    @Test fun shouldFailWhenSensorFolderIsNotPresent(context: TestContext) {
+        val waitingForFailedDeployment = context.async()
+
+        val verticle = TemperatureReadingVerticle(TEST_PATH_LOCATION)
+
+        vertx.deployVerticle(verticle, WORKER_VERTICLE) { deploymentResult ->
+            context.assertFalse(deploymentResult.succeeded())
+            context.assertEquals(deploymentResult.cause().message, "Cannot find sensor Folders and files in location ${TEST_PATH_LOCATION}")
+
+            waitingForFailedDeployment.complete()
+        }
+
+        waitingForFailedDeployment.await(500)
+    }
+
+    @Test fun shouldFailWhenSensorFileIsNotPresent(context: TestContext) {
+        val directory = File(TEST_PATH_LOCATION, DEVICE_NAME_FOLDER)
+        directory.mkdirs()
+        val waitingForFailedDeployment = context.async()
+
+        val verticle = TemperatureReadingVerticle(TEST_PATH_LOCATION)
+
+        vertx.deployVerticle(verticle, WORKER_VERTICLE) { deploymentResult ->
+            context.assertFalse(deploymentResult.succeeded())
+            println(deploymentResult.result())
+            context.assertTrue(deploymentResult.cause().message!!.contains("Sensor file doesn't exist "))
+
+            waitingForFailedDeployment.complete()
+        }
+        waitingForFailedDeployment.await(500)
+    }
+
+    @Test fun shouldSucceedWhenSensorFileIsPresent(context: TestContext) {
         val directory = File(TEST_PATH_LOCATION, DEVICE_NAME_FOLDER)
         directory.mkdirs()
 
@@ -54,35 +138,73 @@ class TemperatureReadingVerticleTest {
             writeText(SAMPLE_VALID_FILE_CONTENT)
         }
 
+        val waitingForFailedDeployment = context.async()
         val verticle = TemperatureReadingVerticle(TEST_PATH_LOCATION)
+        vertx.deployVerticle(verticle, WORKER_VERTICLE) { result ->
+            context.assertTrue(result.succeeded())
+            waitingForFailedDeployment.complete()
+        }
 
-        val result = verticle.readTemperatureFromFile(directory)
-        Assert.assertEquals(20.437f, result)
+        waitingForFailedDeployment.await(500)
     }
 
-    @Test(expected = FileNotFoundException::class) fun shouldFailIfFileDoesntExists(){
-        val directory = File(TEST_PATH_LOCATION, DEVICE_NAME_FOLDER)
-
-        val verticle = TemperatureReadingVerticle(TEST_PATH_LOCATION)
-
-        val result = verticle.readTemperatureFromFile(directory)
-
-        Assert.assertNull(result)
-    }
-
-    @Test fun shouldReturnNullIfBoloxInFile(){
+    @Test fun shouldReadTemperatureContentAndSendEvent(context: TestContext) {
         val directory = File(TEST_PATH_LOCATION, DEVICE_NAME_FOLDER)
         directory.mkdirs()
 
         File(directory, TemperatureReadingVerticle.SENSOR_FILE_NAME).with {
-            writeText("BALLS some lore ipsum shit and stuff")
+            writeText(SAMPLE_VALID_FILE_CONTENT)
         }
-        val verticle = TemperatureReadingVerticle(TEST_PATH_LOCATION)
 
-        val result = verticle.readTemperatureFromFile(directory)
-        Assert.assertNull(result)
+        val verticle = TemperatureReadingVerticle(TEST_PATH_LOCATION, 0.1f)
+        val asyncAssertSuccess = context.asyncAssertSuccess<String>()
+        val waitingForMessage = context.async()
+
+        vertx.eventBus().consumer<JsonObject>(BusAddresses.TemperatureReadings.TEMPERATUR_READING_RECEIVED){ readingMessage ->
+            val jsonObject = readingMessage.body()
+            context.assertEquals(jsonObject.getFloat("temperature"), 20.437f)
+            context.assertEquals(jsonObject.getString("stamp"), "6f")
+
+            waitingForMessage.complete()
+        }
+
+        vertx.deployVerticle(verticle, WORKER_VERTICLE, asyncAssertSuccess)
+        waitingForMessage.await(500)
+
     }
 
+
+    @Test fun shouldSendSensorFailEvent(context: TestContext){
+        val directory = File(TEST_PATH_LOCATION, DEVICE_NAME_FOLDER)
+        directory.mkdirs()
+
+        val sensorFile = File(directory, TemperatureReadingVerticle.SENSOR_FILE_NAME)
+        sensorFile.with {
+            writeText(SAMPLE_VALID_FILE_CONTENT)
+        }
+
+        val verticle = TemperatureReadingVerticle(TEST_PATH_LOCATION, 0.1f)
+        val asyncAssertSuccess = context.asyncAssertSuccess<String>()
+        val waitingForFirstMessage = context.async()
+        val waitinfForFailMessage = context.async()
+
+        vertx.eventBus().consumer<JsonObject>(BusAddresses.TemperatureReadings.TEMPERATUR_READING_RECEIVED) { message ->
+            waitingForFirstMessage.complete()
+            sensorFile.delete()
+        }
+
+        vertx.eventBus().consumer<JsonObject>(BusAddresses.TemperatureReadings.TEMPERATURE_SENSOR_READING_FAILED) { message ->
+            context.assertEquals(message.body().getInteger("code"), 0)
+            context.assertNotNull(message.body().getString("message"))
+            waitinfForFailMessage.complete()
+        }
+
+        vertx.deployVerticle(verticle, WORKER_VERTICLE, asyncAssertSuccess)
+        waitingForFirstMessage.await(500)
+        waitinfForFailMessage.await(1500)
+
+
+    }
 
     val SAMPLE_VALID_FILE_CONTENT = """47 01 ff ff 7f ff ff ff 6f : crc=6f YES
 47 01 ff ff 7f ff ff ff 6f t=20437"""
